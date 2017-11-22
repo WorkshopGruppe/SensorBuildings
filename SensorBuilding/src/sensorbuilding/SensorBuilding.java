196624a38aa0850ff58947f710ed06524a5f75bf
Robinlp <rphjerpsted@gmail.com>
2017-11-22 11:21:57 +0100

    start

---
+++ SensorBuilding/src/sensorbuilding/SensorBuilding.java
@@ -0,0 +1,16 @@
+package sensorbuilding;
+
+/**
+ *
+ * @author Robin
+ */
+public class SensorBuilding {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
