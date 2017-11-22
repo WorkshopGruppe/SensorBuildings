3134b82857a24a297805b23e87758719622033f0
Robinlp <rphjerpsted@gmail.com>
2017-11-22 13:05:39 +0100

    javaFX

---
+++ SensorBuilding/src/UI/FXMLController.java
@@ -0,0 +1,22 @@
+package UI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Robin
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
