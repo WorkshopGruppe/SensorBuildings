78890d99aca716d100fbd8cfe8212aa1a2138919
AAbdo1 <15629756+AAbdo1@users.noreply.github.com>
2017-11-22 13:06:30 +0100

    Update Reading.java

--- SensorBuilding/src/Business/Reading.java
+++ SensorBuilding/src/Business/Reading.java
@@ -3,12 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Business;
+
+import Interface.IReading;
+import java.util.ArrayList;
 
 /**
  *
- * @author Simon
+ * @author AskeWulf
  */
-public class Reading {
+public class Reading implements IReading{
+    private String time;
+    private String value;
+    private String type;
+    private ArrayList<Sensor> senors;
+    
+    Reading(String time, String value, String type) {
+        this.time = time;
+        this.value = value;
+        this.type = type;
+    }
 
+    public String getTime() {
+        return time;
+    }    
+
+    public String getValue(double value) {
+        return value;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setTime() {
+        this.time = time;
+    }
+
+    public void setValue() {
+        this.value = value;
+    }    
+    
+    public void setType() {
+        this.type = type;
+    }    
+    
 }
