38292184090c39e03760a8c78627810b9834c8f1
simonnj <32192443+simonnj@users.noreply.github.com>
2017-11-22 11:56:40 +0100

    sensor

--- SensorBuilding/src/Business/Sensor.java
+++ SensorBuilding/src/Business/Sensor.java
@@ -1,9 +1,18 @@
 package Business;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Robin
  */
 public class Sensor {
     
+    private String Value;
+    private String ID;
+    private ArrayList<Reading> readings;
+    
+    Sensor(String ID, String Value){
+        
+    }
 }
