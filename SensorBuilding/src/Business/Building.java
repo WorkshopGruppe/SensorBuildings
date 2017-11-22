e91b388c365804f87e21b24337390463066e7ea2
Robinlp <rphjerpsted@gmail.com>
2017-11-22 12:15:16 +0100

    building

---
+++ SensorBuilding/src/Business/Building.java
@@ -0,0 +1,42 @@
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Robin
+ */
+public class Building {
+    private String name;
+    private String address;
+    private ArrayList<Sensor> sensors;
+    
+    public Building(String name, String address){
+        this.name = name;
+        this.address = address;
+        this.sensors = new ArrayList<>();
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getAddress(){
+        return this.address;
+    }
+    
+    public ArrayList<Sensor> getSensors(){
+        return this.sensors;
+    }
+    
+    public void addSensor(Sensor sensor){
+        this.sensors.add(sensor);
+    }
+    
+    public void removeSensor(Sensor sensor){
+        this.sensors.remove(sensor);
+    }
+    
+    
+    
+}
